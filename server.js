6baa287b127f1bd22c1aca3f0e7a08e531459608
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-09 01:23:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,51 +62,52 @@ var articles=
 };
 
 function createTemplate (data)
-{
- var title=data.title;
- var heading=data.heading;
- var date=data.date;
- var content=data.content;
+    {
+     var title=data.title;
+     var heading=data.heading;
+     var date=data.date;
+     var content=data.content;
 
 
 
-var htmlTemplate=
-`
-<html>
-    <head>
-        <title>
-        ${title}    
-        </title>
-          <link href="/ui/style.css" rel="stylesheet" />
-        
-    </head>
-    <body>
-        <div class="container">
+    var htmlTemplate=
+    `
+    <html>
+        <head>
+            <title>
+            ${title}    
+            </title>
+              <link href="/ui/style.css" rel="stylesheet" />
             
-       
-        <div>
-            <a href='/'>Home</a>
-        </div>
-        <hr>
-         
-         <h3>
-             ${heading}
-        </h3>
+        </head>
+        <body>
+            <div class="container">
+                
+           
+            <div>
+                <a href='/'>Home</a>
+            </div>
+            <hr>
              
-        <div>
-            ${date}
-        </div>
-        <div>
-            ${content}
-        </div>   
-         </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-}
+             <h3>
+                 ${heading}
+            </h3>
+                 
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>   
+             </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+    }
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res)
+{
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
@@ -121,11 +122,13 @@ app.get('/:articleName', function (req, res)
 
 
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res)
+{
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function (req, res)
+{
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
