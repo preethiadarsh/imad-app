666ce7df4cfc44dacc41939df6cd5448ca9162aa
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-14 20:15:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ Submit.onclick=function()
     // Make a request to the Server and send the name
     
     // Capture the response, ie., the list of names and render it as a list
-    var names=['name1', 'name2', 'name3'];
+    var names=['name1', 'name2', 'name3', 'name4'];
     var list='';
     for (var i=0; i<names.length; i++)
     { 
