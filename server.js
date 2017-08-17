986beb179bcff31faecbd3ad12840ae9d9e30314
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-17 23:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
 
+var config=
+{
+    user: 'preethiadarsh9700',
+    database:'preethiadarsh9700',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -117,10 +127,22 @@ app.get('/', function (req, res)
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool= new Pool(config);
 app.get('/test-db', function (req, res)
 {
   // make a select request
   // return a response with the results
+  pool.query('SELECT * FROM test', function(err, result)
+  {
+      if (err)
+      {
+          res.status(500).send (err.tostring());
+      }
+      else
+      { 
+          res.send(JSON.stringify(result));
+      }
+});
 });
 
 var counter=0;
