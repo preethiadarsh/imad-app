fdeca7ca5e05775c19bf72474a415e281b315391
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-17 23:26:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/test-db', function (req, res)
       }
       else
       { 
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
 });
 });
