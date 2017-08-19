244ea3ca43da29c1c9b530793a9d293ffd7c161d
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-20 00:15:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/counter', function (req, res)
   counter=counter+1;
   res.send(counter.toString());
 });
-
+var names=[];
 app.get('/submit-name', function (req, res)
 {
     // URL:/submit-name?name=xxxxx
