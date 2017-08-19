b47cf933fff5ab8a8253a3a419a597c7f6e63ba9
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-20 00:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -201,7 +201,7 @@ app.get('/articles/:articleName', function (req, res)
       { 
           //res.send(JSON.stringify(result.rows));
       }
-  }
+  };
   //res.send(createTemplate(articleData));
   
 });
