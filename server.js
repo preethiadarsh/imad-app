dff740e20c9c1ba8847331d14397e9ed22cd525f
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-20 01:27:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ function createTemplate (data)
             </h3>
                  
             <div>
-                ${date}
+                ${date.toDateString()}
             </div>
             <div>
                 ${content}
