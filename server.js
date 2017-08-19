c1f939719ae371c2ecc2d0e3dd37a4ce91288672
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-20 01:18:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,61 @@ var config=
 var app = express();
 app.use(morgan('combined'));
 
+var articles=
+{
+
+
+    'article-one':
+    {
+        title: 'Article One: By M.Sivakumar',
+        heading: 'Article One',
+        date: '06 SEP 2017',
+        content:
+        `<p>
+        This is Para-1 of Article One. 
+        </p>
+        <p>
+        This is Para-2 of Article One. 
+        </p>
+        <p>
+        This is Para-3 of Article One. 
+        </p>`
+    },
+
+    'article-two':
+    {
+        title: 'Article Two: By M.Sivakumar',
+        heading: 'Article Two',
+        date: '07 SEP 2017',
+        content:
+        `<p>
+        This is Para-1 of Article Two. 
+        </p>
+        <p> 
+        This is Para-2 of Article Two. 
+        </p>
+        <p>
+        This is Para-3 of Article Two. 
+        </p>`
+    },
+
+    'article-three':
+    {
+        title: 'Article Three: By M.Sivakumar',
+        heading: 'Article Three',
+        date: '08 SEP 2017',
+        content:
+        `<p>
+        This is Para-1 of Article Three. 
+        </p>
+        <p> 
+        This is Para-2 of Article Three. 
+        </p>
+        <p>
+        This is Para-3 of Article Three. 
+        </p>`
+    }
+};
 
 function createTemplate (data)
     {
@@ -151,6 +206,52 @@ app.get('/articles/:articleName', function (req, res)
   
 });
 
+app.get('/ui/style.css', function (req, res)
+{
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res)
+{
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res)
+{
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+//var names=[];
+
+//app.get('/submit-name', function (req, res)
+//{
+    // URL:/submit-name?name=xxxxx
+    // Get the name from the request
+   // var name= req.query.name; //ToDO
+  // names.push(name);
+    //JSON:JavaScript Object Notation
+    
+   //res.send(JSON.stringify(names)); //ToDo
+
+// following code is for request with variable 'name' passed on to server
+//app.get('/submit-name/:name', function (req, res)
+//{
+    // Get the name from the request
+ //   var name= req.params.name; //ToDO
+ //   names.push(name);
+    //JSON:JavaScript Object Notation
+    
+ //   res.send(JSON.stringify(names)); //ToDo
+  
+//});
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+//var port = 80;
+//app.listen(port, function () {
+ // console.log(`IMAD course app listening on port ${port}!`);
+//});
 
 
 
