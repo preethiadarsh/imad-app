fdfe300de2b145e93ea297472a33d80f7a1f797b
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-20 02:10:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,10 +161,10 @@ app.get('/ui/main.js', function (req, res)
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res)
-{
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+//app.get('/ui/madi.png', function (req, res)
+//{
+ // res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+//});
 
 //var names=[];
 
