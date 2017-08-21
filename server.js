766889e53afc1aafbd7225dfb205a96054a929fd
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-21 17:18:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/', function (req, res)
 });
 
 var pool= new Pool(config);
-app.get('/test-db', function (req, res)
+app.get('/test', function (req, res)
 {
   // make a select request
   // return a response with the results
