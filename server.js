a2358038e9361d89f76df34c9661b17ed40392d8
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-23 01:56:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
 var crypto=require('crypto');
-
+var bodyParser = require('body-parser');
 var config=
 {
     user: 'preethiadarsh9700',
@@ -16,7 +16,7 @@ var config=
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 function createTemplate(data)
     {
@@ -91,6 +91,27 @@ app.get('/hash/:input', function (req, res)
 {
     var hashedString=hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res)
+{
+    // username, password
+    // JSON
+    var username = req.body.username;
+    var passwaord = req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password, salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result)
+    {
+       if (err)
+      {
+          res.status(500).send (err.toString());
+      }
+      else
+      { 
+          res.send("User Successfully Created with Username:" + username);
+      } 
+    });
 });
 
 var pool= new Pool(config);
