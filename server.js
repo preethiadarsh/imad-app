065354478732d0817ccd0c3f747c4961370687fb
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-23 02:24:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/hash/:input', function (req, res)
 app.post('/create-user', function(req, res)
 {
     // username, password
-    // {"username": "siva", "password": "xyzabc"}
+    // {"username": "siva", "password": "password"}
     // JSON
     var username = req.body.username;
     var password = req.body.password;
