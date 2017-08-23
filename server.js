c363dd68ece0e8803301129235c4f4e42e7aa64d
preethiadarsh <30705393+preethiadarsh@users.noreply.github.com>
2017-08-23 16:47:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool= require('pg').Pool;
 var crypto=require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var config=
 {
     user: 'preethiadarsh9700',
@@ -17,6 +18,10 @@ var config=
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue', 
+    cookie: {maxAge: 1000*60*60*24*30}
+}));
 
 function createTemplate(data)
     {
@@ -145,8 +150,7 @@ app.post('/login', function(req, res)
               {
                   res.send('Credentials correct');
                   
-                  // Set a Session
-                  
+                 
               }
               else
               {
